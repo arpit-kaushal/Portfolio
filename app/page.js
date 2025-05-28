1fa4be17a11f69a7e45dcc051bdf22ac3aad61d9
arpit-kaushal <156513120+arpit-kaushal@users.noreply.github.com>
2025-05-28 23:56:02 +0530

    navbar

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Navbar from "@/ui/Navbar";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Navbar></Navbar>
       <div>
         <h1 className={styles.name}>Arpit Kaushal</h1>
         <p className={styles.subtitle}>From KIIT Bhubneshwar</p>
---
+++ ui/Navbar/index.js
@@ -0,0 +1,16 @@
+import styles from './styles.module.css'
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            <div>Home</div>
+            <div>About</div>
+            <div>Project</div>
+            <div>Contact</div>
+            
+            
+            
+        </div>
+    )
+}
+
+export default Navbar
